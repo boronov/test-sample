d8a0ab289ea6cc0c5e02c09e36b39b92460e6c7c
Boronov Faridun <fboronov11@gmail.com>
2023-11-21 10:22:38 +0500

    git init

---
+++ app/src/main/java/ru/appsmile/test/hotel/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.appsmile.test.hotel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
